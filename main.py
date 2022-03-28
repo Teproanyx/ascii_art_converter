26be4c30196feef8aedc13a2012592157d35bc30
Teproanyx <poomgammer@gmail.com>
2022-03-28 16:12:11 +0700

    Clean up file names processing.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sys import argv
 import processing
 import re
+from os.path import splitext
 
 option = dict.fromkeys(['-e', '-sa', '-nb', '-bl'], False)
 option['-s'] = (100, 100)
@@ -15,26 +16,27 @@ if __name__ == "__main__":
         print_help()
         quit()
 
-    for i in argv:  # option processing
-        if i[0] == '-':
-            if i.startswith('-s'):  # size option
-                capture = re.match(r'-s(\d+)x(\d+)', i)
+    for index in argv:  # option processing
+        if index[0] == '-':
+            if index.startswith('-s'):  # size option
+                capture = re.match(r'-s(\d+)x(\d+)', index)
                 if capture:
                     option['-s'] = (capture.group(1), capture.group(2))
-            elif i in option:
-                option[i] = True    # other options
+            elif index in option:
+                option[index] = True  # other options
         else:
             break
 
+    quote = '\"\''
     if "-b" not in argv:  # one image
-        image = argv[-2]
+        image = argv[-2].strip(quote)
         output = argv[-1]
-        processing(image, output, option)
-
+        processing(image, splitext(output)[0] + '.txt', option)
     else:  # batch
         app = argv[-1]  # appended text at the end of each files
-        for i in reversed(argv[:-1]):
-            if i[0] != '-':
-                processing(i, f'{i}_{app}', option)
+        for index in reversed(argv[:-1]):
+            if index[0] != '-':
+                image = index.strip(quote)
+                processing(image, f'{splitext(image)[0]}_{app}.txt', option)
             else:
                 break
