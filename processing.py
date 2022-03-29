24d7b09e7b19d50418ada022a18a5e4edb726dc1
Teproanyx <poomgammer@gmail.com>
2022-03-29 18:27:56 +0700

    Implement writing to file

--- processing.py
+++ processing.py
@@ -21,11 +21,14 @@ def transform(val: int, from_max: int, to_max: int) -> int:
 
 
 def processing(img: str, output_name: str, options: dict) -> None:
-    image = Image.open(img).convert("L")
-    og_width, og_height = image.size
-    width = int((options['-s'][0] / 100) * og_width)
-    height = int((options['-s'][1] / 100) * og_height)
-    image.resize((width, height))
+    with Image.open(img).convert("L") as image:
+        og_width, og_height = image.size
+        width = int((options['-s'][0] / 100) * og_width)
+        height = int((options['-s'][1] / 100) * og_height)
+        image = image.resize((width, height))
 
-    if options['-bl']:
-        image = image.filter(ImageFilter.BLUR)
+        if options['-bl']:
+            image = image.filter(ImageFilter.BLUR)
+
+        with open(output_name, "w") as out:
+            out.write(ascii_conversion(image))
