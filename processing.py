ee0b7e67ec73f20c28415e3562c2db2072dd7a60
Teproanyx <poomgammer@gmail.com>
2022-03-30 15:39:31 +0700

    Turn double for loops appending into interpolation, indexing, and list comprehension

--- processing.py
+++ processing.py
@@ -8,17 +8,10 @@ def ascii_conversion(img: Image.Image) -> str:
     char_set = list(r"""$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. """)
     data = np.array(img.getdata(), dtype='u1').reshape(img.height, img.width)
 
-    output = []
-    for row in data:
-        temp = []
-        for index in row:
-            temp.append(char_set[transform(index, MAX_LUMINANCE, len(char_set) - 1)])
-        output.append(''.join(temp))
-    return '\n'.join(output)
+    data = np.rint(np.interp(data, [0, 255], [0, len(char_set) - 1])).astype('u1')  # round interpolated value to int
+    data = np.array(char_set)[data]  # index each pixel to corresponding ascii representation
 
-
-def transform(val: int, from_max: int, to_max: int) -> int:
-    return round(val / from_max * to_max)
+    return '\n'.join([''.join(row) for row in data])    # turn ascii matrix into a multiline string
 
 
 def processing(img: str, output_name: str, options: dict) -> None:
